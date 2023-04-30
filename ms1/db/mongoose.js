70bd37af3ea4b95f8ffe2fc37286810618ae5283
Alcides Albuquerque <43549055+a-albuquerque@users.noreply.github.com>
2023-04-30 18:01:31 -0400

    Update DB

--- ms1/db/mongoose.js
+++ ms1/db/mongoose.js
@@ -1,5 +1,5 @@
 'use strict';
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mongo-user:nGc7crCIbNMzOr25@maincluster.erymi.mongodb.net/sports_up_db', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || 'contact me for collab!', { useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = { mongoose }
